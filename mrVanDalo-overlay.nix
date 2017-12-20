6b96035e521fedece00ed487cc26585ee107208b
Ingolf Wagner <contact@ingolf-wagner.de>
2017-12-20 22:59:20 +1300

    add multiple bitwig version

--- mrVanDalo-overlay.nix
+++ mrVanDalo-overlay.nix
@@ -8,4 +8,18 @@ in
 {
   memo = callPackage ./pkgs/memo { };
   newpencil = callPackage ./pkgs/newpencil { };
+
+  bitwig-studio1 = callPackage ./pkgs/bitwig-studio/bitwig-studio1.nix {
+    inherit (super.gnome2) zenity;
+  };
+
+  bitwig-studio2 = callPackage ./pkgs/bitwig-studio/bitwig-studio2.nix {
+    inherit (super.gnome2) zenity;
+    inherit (self) bitwig-studio1;
+  };
+
+  bitwig-studio = callPackage ./pkgs/bitwig-studio/bitwig-studio2.nix {
+    inherit (super.gnome2) zenity;
+    inherit (self) bitwig-studio1;
+  };
 }
