fad9eab036a8d18dd7a1f709cc84ee925b4df3af
Ingolf Wagner <contact@ingolf-wagner.de>
2018-03-09 17:58:47 +0800

    adept to rtmidi3

--- pkgs/rtmidi/default.nix
+++ pkgs/rtmidi/default.nix
@@ -1,7 +1,9 @@
-{ stdenv, fetchFromGitHub, autoconf, automake, libtool, libjack2, alsaLib, pkgconfig }:
+{ stdenv, fetchFromGitHub, rtmidi }:
 
-stdenv.mkDerivation rec {
+rtmidi.overrideAttrs ( oldAttrs : rec {
+
   version = "3.0.0";
+
   name = "rtmidi-${version}";
 
   src = fetchFromGitHub {
@@ -11,19 +13,4 @@ stdenv.mkDerivation rec {
     sha256 = "1z4sj85vvnmvg4pjjs963ghi69srb63jp5xpck46dcb9wgypdviy";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ autoconf automake libtool libjack2 alsaLib ];
-
-  preConfigure = ''
-    ./autogen.sh --no-configure
-    ./configure
-  '';
-
-  meta = {
-    description = "A set of C++ classes that provide a cross platform API for realtime MIDI input/output";
-    homepage =  http://www.music.mcgill.ca/~gary/rtmidi/;
-    license = stdenv.lib.licenses.mit;
-    maintainers = [ stdenv.lib.maintainers.magnetophon ];
-    platforms = with stdenv.lib.platforms; linux ++ darwin;
-  };
-}
+})
